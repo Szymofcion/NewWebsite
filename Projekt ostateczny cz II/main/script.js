2600ce8db5f807ea21655cd863fd028dfd877d97
Szymofcion <82220216+Szymofcion@users.noreply.github.com>
2021-12-12 23:29:05 +0100

    Add files via upload

--- Projekt ostateczny cz II/main/script.js
+++ Projekt ostateczny cz II/main/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 const nav = document.querySelector(".navbar");
-
+const allNavItems=document.querySelectorAll('.nav-link')
+const navList = document.querySelector('.navbar-collapse')
 function addShadow() {
   if (window.scrollY >= 300) {
     nav.classList.add("shadow-bg");
@@ -8,6 +9,6 @@ function addShadow() {
     nav.classList.remove("shadow-bg");
   }
 }
-
+allNavItems.forEach(item => item.addEventListener('click', () =>navList.classList.remove('show') ))
 window.addEventListener("scroll", addShadow);
 })
